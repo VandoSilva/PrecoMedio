b01f81ac0bb9d42c74d9a574cd6a634d3d60f967
Vando Silva <shdzin@hotmail.com>
2020-11-29 20:42:53 -0300

    Vetor media de precos

---
+++ src/Aplicacao/Programa.java
@@ -0,0 +1,40 @@
+package Aplicacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import Entidades.Produtos;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		Produtos[] vect = new Produtos[n];
+		
+		for (int i=0; i<vect.length; i++) {
+			sc.nextLine();
+			String nome = sc.nextLine();
+			double preco = sc.nextDouble();
+			vect[i] = new Produtos(nome, preco);
+		}
+		
+		double soma = 0.0;
+		for (int i=0; i<vect.length; i++) {
+			soma += vect[i].getPreco(); 
+			}
+		
+		double avg = soma / vect.length;
+		System.out.printf("Preco medio =  %.2f%n", avg);
+		
+		sc.close();
+		
+		
+		
+		
+	}
+
+}
